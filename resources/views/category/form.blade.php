eeb7b5fad7f7aa35e2852b076de09173e2c6df81
Abul-Kalam <aazad7978@gmail.com>
2018-10-20 23:01:42 +0600

    first commit

---
+++ resources/views/category/form.blade.php
@@ -0,0 +1,8 @@
+<div class="form-group">
+    <label for="title">Title</label>
+    <input type="text" class="form-control" id="title" name="title">
+</div>
+<div class="form-group">
+    <label for="description">Description</label>
+    <textarea type="text" class="form-control" id="description" name="description" cols="20" rows="5"></textarea>
+</div>
